cfe82a06daaa2d5ae66307bdbd132b6feeb0f512
Charly <pupierchar@cy-tech.fr>
2024-05-14 20:22:42 +0200

    main

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include "historique.c"
 #include "statistiques.c"
 
-
 void debutProgramme(Entrainement entrainement, FILE *file) {
     int choix;
     printf("1. Entrer et sauvegarder les informations/résultats d'un nouvel entrainement\n");
