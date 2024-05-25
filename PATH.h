dc0b38dedad4e48daeedb126c9dc6b6b40bc9536
Charly <166821960+charlyppr@users.noreply.github.com>
2024-05-25 03:05:22 +0200

    ...

--- PATH.h
+++ PATH.h
@@ -1,7 +1,6 @@
 #ifndef PATH_H
 #define PATH_H
 
-#define PATH "/Users/charly/Documents/École/Info/CER-JO"
-//"/Users/Prénom/Documents/CER-JO"
+#define PATH "/Users/Prénom/Documents/CER-JO"
 
 #endif
